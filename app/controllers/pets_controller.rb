4714a532a4e2c628053a5d066ed1891ae4f9e37e
eric <eric.ou@yale.edu>
2020-06-17 08:39:39 -0700

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,12 +7,8 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
-  end
-
-  post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +16,36 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    @pet = find_pet()
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  post '/pets' do 
+    owner = params[:owner_id] ? 
+            find_owner() : 
+            Owner.create(name: params[:create_owner])
+    @pet = Pet.create(name: params[:pet_name], owner_id: owner.id)
     redirect to "pets/#{@pet.id}"
   end
+
+  patch '/pets/:id' do 
+    pet = find_pet()
+    owner = params[:new_owner_name] != "" ? 
+            Owner.create(name: params[:new_owner_name]) : 
+            Owner.find_by(name: params[:owner][:name])
+            
+    pet.update(name: params[:pet_name], owner_id: owner.id)
+    redirect to "pets/#{@pet.id}"
+  end
+
+  def find_pet()
+    Pet.find(params[:id])
+  end
+
+  def find_owner()
+    Owner.find(params[:owner_id])
+  end
+
 end
